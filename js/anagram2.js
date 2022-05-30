a3a6126e813ec1dda2028bbb3b5ca6ae496f18b5
Garrett Adams <garrett.adams52@gmail.com>
2022-05-30 17:05:41 -0400

    finished js

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,22 @@
 exports.anagramsFor = function(word, listOfWords) {
 
+    word = word.split('').sort()
+    ans = []
+    let temp
+    for(let i = 0; i<listOfWords.length; i++){
+        temp = listOfWords[i]
+
+        if(word.join()===listOfWords[i].split('').sort().join())
+            ans.push(temp)
+        
+    }
+
+
+    return ans
+
 };
+
+
+
+//listOfWords = ["threads", "trashed", "hardest", "hatreds", "hounds"];
+//console.log(anagramsFor("threads", listOfWords))
